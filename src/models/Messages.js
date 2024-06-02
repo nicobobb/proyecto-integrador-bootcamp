ec02d536397dfc12083c1b60829854c75560504c
Nico Bobb <nicobobb@gmail.com>
2024-06-02 16:53:13 -0300

    modelo para mensajes

---
+++ src/models/Messages.js
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+const MessagesSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        subject: {
+            type: String,
+            required: true,
+        },
+        body: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const Messages = model("Messages", MessagesSchema);
